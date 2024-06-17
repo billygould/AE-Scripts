66e6042888053eef05a4c59a106560c781fb9ab6
Billy Gould <billygould@Billys-MBP-2.lan1>
2024-06-17 19:44:32 -0400

    adds camera-hit.jsx and layer-by-layer-export.jsx to git repo.

---
+++ camera-hit.jsx
@@ -0,0 +1,76 @@
+function cameraHit() {
+    //get Comp
+
+    var comp = app.project.activeItem;
+
+    if (comp instanceof CompItem) {
+        //get Current Layer and Effect Start time
+        var currentLayer = comp.selectedLayers[0];
+        if (currentLayer) {
+            var effectStart = currentLayer.time;
+        } else {
+            alert("Please Select a Layer");
+            return;
+        }
+
+        //Get user input for intensity and durationd
+        var intensity = 2
+        var duration = 1.5
+
+        //Check if currentLayer is parented to a null layer
+        var isParentedToNull = false;
+
+        if (currentLayer.parent !== null && currentLayer.parent instanceof AVLayer && currentLayer.parent.nullLayer) {
+            isParentedToNull = true;
+        }
+
+        if (!isParentedToNull) {
+            //Add null at effectStart and parent to currentLayer
+            var newNull = comp.layers.addNull(duration);
+            newNull.name = "CamShake_Null"
+            newNull.startTime = effectStart;
+            currentLayer.parent = newNull;
+
+            //set Expressions
+            var sliderExpression = 'effect("Slider Control")(1)';
+            var wiggleExpression = 'wiggle(15,10*effect("Slider Control")("Slider"))';
+            var rotateExpression = 'transform.rotation';
+
+            //add Slider Control and its Expression
+            var sliderEffect = newNull.property("Effects").addProperty("Slider Control");
+            var sliderProperty = sliderEffect.property("Slider");
+            sliderProperty.expression = sliderExpression;
+
+            //add Wiggle and Rotation Expressions
+            newNull.property("Position").dimensionsSeparated = true;
+            newNull.property("Transform").property("X Position").expression = wiggleExpression;
+            newNull.property("Transform").property("Y Position").expression = wiggleExpression;
+
+            newNull.property("Transform").property("Rotation").expression = rotateExpression;
+
+            //add repeTile
+            currentLayer.property("Effects").addProperty("CC RepeTile")
+            //set repeTile values
+            var repeTile = currentLayer.property("Effects").property("CC RepeTile")
+            for (var i = 1; i <= 4; i++) {
+                repeTile.property(i).setValue(300)
+            }
+        } else {
+            var sliderProperty = currentLayer.parent.property("Effects").property("Slider Control").property("Slider");
+        }
+
+        //place Keyframes
+        sliderProperty.setValueAtTime(effectStart, 0.00);
+        sliderProperty.setValueAtTime((effectStart + (duration / 3)), intensity);
+        sliderProperty.setValueAtTime((effectStart + duration), 0.00);
+
+        // Apply Easy Ease to the keyframes
+        sliderProperty.setTemporalEaseAtKey(1, [new KeyframeEase(0.33, 33)], [new KeyframeEase(0.33, 33)]);
+        sliderProperty.setTemporalEaseAtKey(2, [new KeyframeEase(0.33, 33)], [new KeyframeEase(0.33, 33)]);
+        sliderProperty.setTemporalEaseAtKey(3, [new KeyframeEase(0.33, 33)], [new KeyframeEase(0.33, 33)]);
+    } else {
+        alert("Please select a comp.")
+    }
+}
+
+cameraHit();
---
+++ layer-by-layer-export.jsx
@@ -0,0 +1,26 @@
+alert("Click OK to execute layer by layer.");
+
+var comp = app.project.item(1);
+var renderQueue = app.project.renderQueue;
+
+var destination = Folder.selectDialog("Select Destination");
+var fileName = prompt("Please Enter The File Name.");
+
+for (var i = 1; i <= comp.layers.length; i++) {
+    var currentLayer = comp.layer(i);
+
+    if(currentLayer.enabled == true && currentLayer.selected == true) {
+
+        currentLayer.solo = true;
+        
+        var renderQueueItem = renderQueue.items.add(comp);
+
+        var outputModule = renderQueueItem.outputModule(1);
+        outputModule.file = new File(String(destination) + "/" + fileName + i + ".mov");
+
+        renderQueue.render();
+
+        currentLayer.solo = false;
+    }
+}
+
